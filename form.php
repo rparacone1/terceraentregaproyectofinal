694c1148bcb38ce5871c26a52a8354c66d9c5b60
Renato Paracone <renato.paracone@hotmail.com>
2022-05-29 20:15:26 -0300

    ultimos cambios

---
+++ form.php
@@ -0,0 +1,21 @@
+<?php
+// Capturo info que se ingresa en imputs del formulario //
+
+$nombre = $_POST['nombre'];
+$mail = $_POST['email'];
+$mensaje = $_POST['textarea'];
+
+
+// destinatario //
+
+$para = 'renato.paracone@hotmail.com';
+$asunto = 'Este mail fue enviado desde la web';
+
+// Funcion que envia la info del form//
+mail($para, $asunto, utf8_decode($nombre, $mail, $mensaje));
+
+header('Location:exito.html')
+
+
+
+?>
